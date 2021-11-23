f7a10ad4743cc1d26c6f8252b9382460603e5b89
Salih Bayram <alicamci2020@gmail.com>
2021-11-23 23:55:36 +0300

    V3

--- src/main/java/com/lecture/car_rental/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/lecture/car_rental/security/service/UserDetailsServiceImpl.java
@@ -8,16 +8,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
 @AllArgsConstructor
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user= userRepository
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return UserDetailsImpl.build(user);
     }
 }
