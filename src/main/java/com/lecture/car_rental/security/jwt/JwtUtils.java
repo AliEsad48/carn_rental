f8aefed7cc181b84171fc563b4c37d209ff0dca5
Salih Bayram <alicamci2020@gmail.com>
2021-11-24 00:17:05 +0300

    V5

---
+++ src/main/java/com/lecture/car_rental/security/jwt/JwtUtils.java
@@ -0,0 +1,36 @@
+package com.lecture.car_rental.security.jwt;
+
+import com.lecture.car_rental.security.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${backendapi.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${backendapi.app.jwtExpirationMs}")
+    private long jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userDetails.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
