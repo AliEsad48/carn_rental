d159409b523d9add94be8c69ecfd61215f6d88af
Salih Bayram <alicamci2020@gmail.com>
2021-11-23 23:58:37 +0300

    V3

--- src/main/java/com/lecture/car_rental/repository/UserRepository.java
+++ src/main/java/com/lecture/car_rental/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.lecture.car_rental.repository;
 
 import com.lecture.car_rental.domain.User;
+import com.lecture.car_rental.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByUsername(String username);
-
+    Optional<User> findByUsername(String username) throws ResourceNotFoundException;
 }
