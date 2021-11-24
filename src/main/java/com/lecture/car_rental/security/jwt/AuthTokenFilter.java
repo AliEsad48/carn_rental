87e15ec9a5fa35109fff5b73c171bd34cd76f7fb
Salih Bayram <alicamci2020@gmail.com>
2021-11-24 22:21:56 +0300

    V12

--- src/main/java/com/lecture/car_rental/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/lecture/car_rental/security/jwt/AuthTokenFilter.java
@@ -17,10 +17,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
+
     @Autowired
     private JwtUtils jwtUtils;
+
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
     @Override
@@ -29,23 +32,35 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         try {
             String jwt = parseJwt(request);
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+
                 String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
                 request.setAttribute("username", username);
+
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
                 UsernamePasswordAuthenticationToken authentication = new
                         UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception e) {
+
             logger.error("Cannot set user authentication: {}", e);
+
         }
     }
 
     private String parseJwt(HttpServletRequest request) {
+
         String headerAuth = request.getHeader("Authorization");
+
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+
             return headerAuth.substring(7);
+
         }
+
         return null;
     }
 }
